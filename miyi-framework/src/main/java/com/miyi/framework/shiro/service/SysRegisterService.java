47aa8dbc6ec9a5f57b5605f4b433ad158f55fa1f
Administrator <524559124@qq.com>
2025-03-13 23:15:49 +0800

    项目初始化

---
+++ miyi-framework/src/main/java/com/miyi/framework/shiro/service/SysRegisterService.java
@@ -0,0 +1,83 @@
+package com.miyi.framework.shiro.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import com.miyi.common.constant.Constants;
+import com.miyi.common.constant.ShiroConstants;
+import com.miyi.common.constant.UserConstants;
+import com.miyi.common.core.domain.entity.SysUser;
+import com.miyi.common.utils.DateUtils;
+import com.miyi.common.utils.MessageUtils;
+import com.miyi.common.utils.ServletUtils;
+import com.miyi.common.utils.ShiroUtils;
+import com.miyi.common.utils.StringUtils;
+import com.miyi.framework.manager.AsyncManager;
+import com.miyi.framework.manager.factory.AsyncFactory;
+import com.miyi.system.service.ISysUserService;
+
+/**
+ * 注册校验方法
+ * 
+ * @author ruoyi
+ */
+@Component
+public class SysRegisterService
+{
+    @Autowired
+    private ISysUserService userService;
+
+    @Autowired
+    private SysPasswordService passwordService;
+
+    /**
+     * 注册
+     */
+    public String register(SysUser user)
+    {
+        String msg = "", loginName = user.getLoginName(), password = user.getPassword();
+
+        if (ShiroConstants.CAPTCHA_ERROR.equals(ServletUtils.getRequest().getAttribute(ShiroConstants.CURRENT_CAPTCHA)))
+        {
+            msg = "验证码错误";
+        }
+        else if (StringUtils.isEmpty(loginName))
+        {
+            msg = "用户名不能为空";
+        }
+        else if (StringUtils.isEmpty(password))
+        {
+            msg = "用户密码不能为空";
+        }
+        else if (password.length() < UserConstants.PASSWORD_MIN_LENGTH
+                || password.length() > UserConstants.PASSWORD_MAX_LENGTH)
+        {
+            msg = "密码长度必须在5到20个字符之间";
+        }
+        else if (loginName.length() < UserConstants.USERNAME_MIN_LENGTH
+                || loginName.length() > UserConstants.USERNAME_MAX_LENGTH)
+        {
+            msg = "账户长度必须在2到20个字符之间";
+        }
+        else if (!userService.checkLoginNameUnique(user))
+        {
+            msg = "保存用户'" + loginName + "'失败，注册账号已存在";
+        }
+        else
+        {
+            user.setPwdUpdateDate(DateUtils.getNowDate());
+            user.setUserName(loginName);
+            user.setSalt(ShiroUtils.randomSalt());
+            user.setPassword(passwordService.encryptPassword(loginName, password, user.getSalt()));
+            boolean regFlag = userService.registerUser(user);
+            if (!regFlag)
+            {
+                msg = "注册失败,请联系系统管理人员";
+            }
+            else
+            {
+                AsyncManager.me().execute(AsyncFactory.recordLogininfor(loginName, Constants.REGISTER, MessageUtils.message("user.register.success")));
+            }
+        }
+        return msg;
+    }
+}
